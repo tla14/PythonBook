2747fc10e74b71ed65e5e91d8158720eabfb2f85
tla14 <tla14@students.ptcollege.edu>
2020-12-18 20:18:56 -0500

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -112,6 +112,6 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
+        
         # Satisfied, they both go back to sleep
 
