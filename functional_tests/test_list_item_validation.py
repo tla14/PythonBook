3b50e2ac5f4e6e54fe0342ded127932c2d66606a
unknown <tla14@students.ptcollege.edu>
2021-01-08 18:17:14 -0500

    Forgot to commit FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
@@ -19,8 +22,9 @@ class ItemValidationTest(FunctionalTest):
 
         # She starts typing some text for the new item and the error disappears
         self.get_item_input_box().send_keys('Buy milk')
-        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-            '#id_text:valid'
+        self.wait_for(lambda: self.assertEqual(
+            self.get_error_element().text,
+            "You've already got this in your list"
         ))
 
         # And she can submit it successfully
@@ -32,14 +36,14 @@ class ItemValidationTest(FunctionalTest):
 
         # Again, the browser will not comply
         self.wait_for_row_in_list_table('1: Buy milk')
-        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-            '#id_text:invalid'
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
         ))
 
         # And she can correct it by filling some text in
         self.get_item_input_box().send_keys('Make tea')
-        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
-            '#id_text:valid'
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
@@ -61,4 +65,25 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You've already got this in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.browser.find_element_by_css_selector('.has-error').is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.browser.find_element_by_css_selector('.has-error').is_displayed()
         ))
