4cc468b5bfbf44b3955194889cb3079a416a6bea
unknown <tla14@students.ptcollege.edu>
2021-01-08 18:20:32 -0500

    End of Chapter 16

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,6 @@
-var initialize = function () {
-    $('input[name="text"]').on('keypress', function () {
-      $('.has-error').hide();
-    });
-  };
+window.Superlists = {};
+window.Superlists.initialize = function () {
+  $('input[name="text"]').on('keypress', function () {
+    $('.has-error').hide();
+  });
+};
